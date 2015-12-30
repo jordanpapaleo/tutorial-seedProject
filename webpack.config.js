953da87f06ea06199dd53d610d6f61fed6de59cd
Jordan Papaleo <papaleowebdev@gmail.com>
2015-12-30 13:12:31 -0800

    Lint error

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
